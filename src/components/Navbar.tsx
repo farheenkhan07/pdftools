e1abeb61299545eec30a3ccfe2de47ed519a4a62
Farheen <farheenkhan07@github.com>
2026-05-24 12:58:00 -0400

    change name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
             <div className="w-8 h-8 bg-indigo-600 rounded-lg flex items-center justify-center">
               <FileText className="w-5 h-5 text-white" />
             </div>
-            PDFToolsByFarheenKhan
+            PDFToolsByFarheen
           </Link>
 
           {/* Desktop nav */}
